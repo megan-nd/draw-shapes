e4e871fd2a388421b3c2f5bf5df8f6765a4b2822
Megan Baig <git@megs.bulldy80.com>
2015-07-14 18:48:35 -0400

    Created draw_circles; Set limit on draw_triangles; Moved to draw-shapes

--- draw_circles.py
+++ draw_circles.py
@@ -9,59 +9,52 @@ t.sety(260)
 t.down()
 t.speed(0)
 
-side_length = 0
-
-def triangle():
-    for x in range(1,4):
-        t.forward(side_length)
-        t.left(360/3)
+def circle(radius):
+    t.circle(radius)
     t.up()
-    t.forward(side_length+10)
+    t.forward(radius*2+5)
     t.down()
 
-def draw_triangles():
-    global side_length
-    print('How many triangles do you want?')
+def draw_circles():
+    fixed_radius = 15
+    print('How many circles do you want?')
     ans = int(sys.stdin.readline())
     if ans <= 10:
         print('How big?')
         ans2 = int(sys.stdin.readline())
-        side_length = ans2
-        if side_length <= 50:  #px
+        radius = ans2
+        if radius <= 30:  #px
             for x in range(1,ans+1):
-                triangle()
+                circle(radius)
         else:
             print('That\'s too big')
             return
     elif ans >= 11 and ans <=15:  #can fit 15 in a row
         print('Ok! Here we go!')
-        side_length = 30
         for x in range(1,ans+1):
-            triangle()
+            circle(fixed_radius)
     elif ans >= 16 and ans <=30:
         print('Ok! Here we go!')
-        side_length = 30
         for x in range(1,16):
-            triangle()
+            circle(fixed_radius)
         draw_again()
         for x in range(1,(ans-15)+1):
-            triangle()
+            circle(fixed_radius)
     elif ans >= 31:
         print('Wow.. that\'s a lot.. but ok!')
-        side_length = 30
         for x in range(1,(int(ans/15+1))):
-            one_row()
+            one_row(fixed_radius)
             draw_again()
         for x in range(1,(ans-(15*(int(ans/15)))+1)):
-            triangle()
+            circle(fixed_radius)
         print(ans-(15*(int(ans/15)))+1)
     elif ans > 180:
         print('This canvas isn\'t big enough for that.. sorry')
         return
 
-def one_row():
+def one_row(radius):
     for x in range(1,16):
-        triangle()
+        circle(radius)
         
 def draw_again():
     t.up()
@@ -76,7 +69,7 @@ def again():
         print('Press enter to draw again')
         input()
         draw_again()
-        draw_triangles() 
+        draw_circles() 
 
-draw_triangles()
+draw_circles()
 again()
